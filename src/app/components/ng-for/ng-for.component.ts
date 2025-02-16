be3ab82c738d163c1584f08aa17054397a0939ce
Vishwajeet <vishwajeetku987@gmail.com>
2025-02-16 20:40:42 +0530

    implementing pipes

--- src/app/components/ng-for/ng-for.component.ts
+++ src/app/components/ng-for/ng-for.component.ts
@@ -1,17 +1,18 @@
-import { NgFor } from '@angular/common';
+import { NgFor, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { NaPipe } from '../../pipes/na.pipe';
 
 @Component({
   selector: 'app-ng-for',
-  imports: [NgFor],
+  imports: [NgFor, UpperCasePipe, NaPipe],
   templateUrl: './ng-for.component.html',
   styleUrl: './ng-for.component.css'
 })
 export class NgForComponent {
   emplyeeArray:any[] =[
-    {empId:121, name:"aaaa",  city:'pune', contactNo:"23414343"},
-    {empId:121, name:"aaaa",  city:'pune', contactNo:"23414343"},
-    {empId:121, name:"aaaa",  city:'pune', contactNo:"23414343"},
+    {empId:121, name:"aaaa",  city:'', contactNo:"23414343"},
+    {empId:121, name:"",  city:'pune', contactNo:"23414343"},
+    {empId:121, name:null,  city:'pune', contactNo:"23414343"},
     {empId:121, name:"aaaa",  city:'pune', contactNo:"23414343"},
     {empId:121, name:"aaaa",  city:'pune', contactNo:"23414343"}
 
